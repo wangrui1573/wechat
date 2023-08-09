340dc165d4d6124fdea10b0a69fac7928ef0ee54
real233 <real-wang@qq.com>
2023-08-09 11:30:03 +0800

    tools

---
+++ WxRssReader/pages/tools/tools.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    tools: [
+      { name: '工具1', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/vercel.svg', url: '/pages/ip/ip' },
+      { name: '工具2', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/ssh.svg', url: '/pages/tool2/index' },
+      // 添加更多工具...
+    ],
+    tools2: [
+      { name: '工具3', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/traffic.svg', url: '/pages/tool3/index' },
+      { name: '工具4', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/emby.svg', url: '/pages/tool4/index' },
+      // 添加更多工具...
+    ],
+  },
+});

ff09fd5e78c30b325f6ff8f9a9db0270213567d5
real233 <real-wang@qq.com>
2023-08-22 11:33:46 +0800

    music next

--- WxRssReader/pages/music/music.js
+++ WxRssReader/pages/music/music.js
@@ -1,17 +1,17 @@
+// pages/music/music.js
 Page({
   data: {
     musicData: [], // 歌曲数据
     currentMusicIndex: 0, // 当前播放歌曲的索引
     isPlaying: false, // 是否正在播放
-    rotateDeg: 0 // 旋转角度
+    rotateDeg: 0, // 旋转角度
+    playMode: 'sequence' // 播放模式：'sequence'为顺序播放，'random'为随机播放，'loop'为循环播放
   },
 
   onLoad: function () {
-    // 获取远程歌曲数据并更新到本地
     this.updateMusicData();
   },
 
-  // 获取远程歌曲数据并更新到本地
   updateMusicData: function () {
     wx.request({
       url: 'https://db.real9.cn/real/lesson/2',
@@ -33,38 +33,70 @@ Page({
     });
   },
 
-  
+  togglePlayMode: function () {
+    let newMode;
+    if (this.data.playMode === 'sequence') {
+      newMode = 'random';
+    } else if (this.data.playMode === 'random') {
+      newMode = 'loop';
+    } else {
+      newMode = 'sequence';
+    }
 
-  // 播放按钮点击事件
+    this.setData({
+      playMode: newMode
+    });
+  },
+
+  getNextIndex: function () {
+    const currentIndex = this.data.currentMusicIndex;
+    const musicDataLength = this.data.musicData.length;
+
+    if (this.data.playMode === 'sequence') {
+      return currentIndex < musicDataLength - 1 ? currentIndex + 1 : 0;
+    } else if (this.data.playMode === 'random') {
+      return Math.floor(Math.random() * musicDataLength);
+    } else {
+      return currentIndex;
+    }
+  },
+
+  onMusicEnd: function () {
+    const nextIndex = this.getNextIndex();
+    this.setData({
+      currentMusicIndex: nextIndex
+    });
+
+    if (this.data.isPlaying) {
+      this.play();
+    }
+  },
+
   play: function () {
     const music = this.data.musicData[this.data.currentMusicIndex];
 
-    // 播放音乐
     wx.playBackgroundAudio({
       dataUrl: music.link,
       title: music.name
     });
 
-    // 设置播放状态和旋转效果
     this.setData({
       isPlaying: true
     });
     this.rotateCover();
+
+    wx.onBackgroundAudioStop(this.onMusicEnd);
   },
 
-  // 暂停按钮点击事件
   pause: function () {
-    // 暂停音乐
     wx.pauseBackgroundAudio();
 
-    // 设置播放状态和停止旋转
     this.setData({
       isPlaying: false
     });
     clearInterval(this.rotateInterval);
   },
 
-  // 旋转封面图效果
   rotateCover: function () {
     this.rotateInterval = setInterval(() => {
       this.setData({
@@ -73,29 +105,33 @@ Page({
     }, 30);
   },
 
-  // 上一首按钮点击事件
   prev: function () {
     const prevIndex = this.data.currentMusicIndex - 1;
+    let newIndex;
+
     if (prevIndex >= 0) {
-      this.setData({
-        currentMusicIndex: prevIndex
-      });
-      if (this.data.isPlaying) {
-        this.play();
-      }
+      newIndex = prevIndex;
+    } else {
+      newIndex = this.data.musicData.length - 1;
+    }
+
+    this.setData({
+      currentMusicIndex: newIndex
+    });
+
+    if (this.data.isPlaying) {
+      this.play();
     }
   },
 
-  // 下一首按钮点击事件
   next: function () {
-    const nextIndex = this.data.currentMusicIndex + 1;
-    if (nextIndex < this.data.musicData.length) {
-      this.setData({
-        currentMusicIndex: nextIndex
-      });
-      if (this.data.isPlaying) {
-        this.play();
-      }
+    const nextIndex = this.getNextIndex();
+    this.setData({
+      currentMusicIndex: nextIndex
+    });
+
+    if (this.data.isPlaying) {
+      this.play();
     }
   }
 });

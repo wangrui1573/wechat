1d0227e0c69690719bb5893f533e09850ffc0316
real233 <real-wang@qq.com>
2023-08-22 15:20:10 +0800

    图标

--- WxRssReader/pages/tools/tools.js
+++ WxRssReader/pages/tools/tools.js
@@ -1,24 +1,22 @@
 Page({
   data: {
     tools: [
-      { name: 'IP计算器', icon: 'https://gist.githubusercontent.com/wangrui1573/70c9954014e1ebdf57727e20a66918f8/raw/ip.svg', url: '/pages/ip/ip' },
-      { name: '手持弹幕', icon: 'https://gist.githubusercontent.com/wangrui1573/70c9954014e1ebdf57727e20a66918f8/raw/sub.svg', url: '/pages/sub/sub' },
+      { name: 'IP计算器', icon: 'wx-rss', url: '/pages/ip/ip' },
+      { name: '手持弹幕', icon: 'wx-danmu', url: '/pages/sub/sub' },
       // 添加更多工具...
     ],
     tools2: [
-      { name: '工具3', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/traffic.svg', url: '/pages/music/music' },
-      { name: '工具4', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/emby.svg', url: '/pages/anki/anki_real' },
-      { name: '工具5', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/emby.svg', url: '/pages/anki/anki3?lesson=20' },
+      { name: '音乐', icon: 'wx-music', url: '/pages/music/music' },
+      { name: '教育', icon: 'wx-jiaoyu', url: '/pages/anki/anki_real' },
+      { name: '工具5', icon: 'wx-danmu', url: '/pages/anki/anki3?lesson=20' },
       // 添加更多工具...
     ],
     tools3: [
-      { name: 'QB下载', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/emby.svg', appid: 'wxb22773ad9e55876e', path: 'pages/server/server', extraData: { foo: 'bar' }, envVersion: 'release' },
-      { name: '数独', icon: 'https://gist.githubusercontent.com/real233/c00d9431cd79c160692016849e96f805/raw/emby.svg', appid: 'wx9b87be8614be1b5e', path: 'index', extraData: { foo: 'bar' }, envVersion: 'release' },
+      { name: 'QB下载', icon: 'wx-qbittorrent', appid: 'wxb22773ad9e55876e', path: 'pages/server/server', extraData: { foo: 'bar' }, envVersion: 'release' },
+      { name: '数独', icon: 'wx-wode-shududaochu', appid: 'wx9b87be8614be1b5e', path: 'index', extraData: { foo: 'bar' }, envVersion: 'release' },
 
     ]
   },
-
-  
 
   openMiniProgram: function (event) {
     const index = event.currentTarget.dataset.index;
@@ -43,13 +41,6 @@ Page({
       console.error('未找到对应的工具');
     }
   },
-
-
-
-
-
-
-
 
   /**
    * 生命周期函数--监听页面初次渲染完成
